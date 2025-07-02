114815c500a68ad7889ab2403c6eadbfb956e987
Anuragverma0002 <vermaanurag0209@gmail.com>
2025-07-02 17:30:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-   "https://memofold.vercel.app" 
+  https://memofold-frontend-0001.vercel.app
 
 ];
 
