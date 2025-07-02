283a18c65524ee20513e10c6807674bb3a4f6fa0
Anuragverma0002 <vermaanurag0209@gmail.com>
2025-07-02 16:53:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,9 @@ const allowedOrigins = [
   "http://localhost:5500",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
-  "http://127.0.0.1:3000"
+  "http://127.0.0.1:3000",
+   "https://memofold.vercel.app" 
+
 ];
 
 app.use(cors({
