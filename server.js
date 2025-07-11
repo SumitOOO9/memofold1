54d1805f74eaf279a48a2cf833cf86d90bb478dd
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2025-07-11 12:50:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ if (!MONGO_URI) {
 
 // ✅ Safe CORS Setup for Development and Production
 const allowedOrigins = [
-  "http://localhost:5500",
-  "http://127.0.0.1:5500",
+  "http://localhost:5000",
+  "http://127.0.0.1:5000",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "https://memofold-coral.vercel.app"
