dd9f2b79767c0849e311a8f0f11c9dca89caf5cd
Sarthak <Sarthak@exato.ai>
2025-10-06 13:19:30 +0530

    notification done

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -5,7 +5,7 @@ const notificationController = require("../controllers/notificationController");
 const router = express.Router();
 
 router.get("/", authenticate, notificationController.getNotifications);
-router.put("/notification/:id/read", authenticate, notificationController.markAsRead);
+router.put("/notification/read/:id", authenticate, notificationController.markAsRead);
 router.get("/unread-count", authenticate, notificationController.getUnreadCount);
 
 module.exports = router;
