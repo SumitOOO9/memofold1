d1330df65c9dc08f13cfd9449ec5259fbfc249b5
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2025-10-09 01:28:48 +0530

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { authenticate } = require("../middleware/authMiddleware");
-const { getStreamToken } = require("../controllers/getStreamToken");
+const { getStreamToken } = require("../controllers/chatController");
 const { ensureUsersExist } = require("../controllers/ensureUsersController");
 
 const router = express.Router();
