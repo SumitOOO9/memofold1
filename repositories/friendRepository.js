380a6e0b5b8196327b0024921dd5c5b74df9dfa5
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2025-12-28 23:58:41 +0530

    Update friendRepository.js

--- repositories/friendRepository.js
+++ repositories/friendRepository.js
@@ -37,7 +37,7 @@ class FriendRepository {
 
   static async getfriendBYFriendId(userId, otherUserId) {
     const friend = await FriendList.findOne({ user: userId }, {
-      friends: { $elemMatch: { friendId: otherUserId } }
+      friends: { $elemMatch: { _id: otherUserId } }
     });
     console.log("friend in repo",friend);
     return friend;
