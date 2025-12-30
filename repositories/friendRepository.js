b900b5edd5e1e4b39f618291dbf5d7483160b315
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2025-12-30 19:39:53 +0530

    Update friendRepository.js

--- repositories/friendRepository.js
+++ repositories/friendRepository.js
@@ -1,7 +1,6 @@
 const User = require('../models/user');
 const Notification = require('../models/notification');
 const FriendList = require('../models/friendList');
-const FriendList = require('../models/friendList');
 
 class FriendRepository {
   static async saveUser(user) {
